41531bc65655f79608519ac0caa9df5d636af91f
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-07-14 11:17:07 +0500

    bug fixes

--- src/sevices/axios.js
+++ src/sevices/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { API_URL } from "../consts";
 
-export const axiosInstance = axios.create({
+export let axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem("token" || "")}`,
+    Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
   },
 });
